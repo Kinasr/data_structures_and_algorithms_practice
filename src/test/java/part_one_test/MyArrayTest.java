71fef935fe1c3cb35e2d134f9b1e5864f71712d3
Khaled Nasr <kinasr95@gmail.com>
2021-12-02 18:10:03 +0200

    solve the issue

--- src/test/java/part_one_test/MyArrayTest.java
+++ src/test/java/part_one_test/MyArrayTest.java
@@ -1,6 +1,8 @@
 package part_one_test;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import part_one.MyArray;
 
@@ -17,60 +19,74 @@ public class MyArrayTest {
         numbers.insert(3);
     }
 
-    @Test
-    void insertNumberWithinTheFixedLength() {
-        assertEquals(3, numbers.length());
-        assertEquals(2, numbers.get(1));
+    @Nested
+    class InsertTest {
+        @Test @Tag("Smoke") @Tag("Regression")
+        void insertNumberWithinTheFixedLength() {
+            assertEquals(3, numbers.length());
+            assertEquals(2, numbers.get(1));
+        }
+
+        @Test @Tag("Smoke") @Tag("Regression")
+        void insertNumberAboveTheFixedLength() {
+            numbers.insert(4);
+
+            assertEquals(4, numbers.length());
+            assertEquals(4, numbers.get(3));
+        }
+
+        @Test @Tag("Smoke") @Tag("Regression")
+        void insertNumberAfterRemoveOne() {
+            numbers.removeAt(0);
+            numbers.insert(4);
+
+            assertAll(
+                    () -> assertEquals(3, numbers.length()),
+                    () -> assertEquals(4, numbers.get(2))
+            );
+        }
     }
 
-    @Test
-    void insertNumberAboveTheFixedLength() {
-        numbers.insert(4);
+    @Nested
+    class RemoveAtTest {
+        @Test @Tag("Smoke") @Tag("Regression")
+        void removeNumber() {
+            numbers.removeAt(0);
 
-        assertEquals(4, numbers.length());
-        assertEquals(4, numbers.get(3));
+            assertEquals(2, numbers.length());
+            assertEquals(2, numbers.get(0));
+        }
+
+        @Test @Tag("Regression")
+        void removeNumberOutOfArrayLength() {
+            assertAll(
+                    () -> assertThrows(IllegalArgumentException.class, () -> numbers.removeAt(10)),
+                    () -> assertEquals(3, numbers.length())
+            );
+        }
+
+        @Test @Tag("Regression")
+        void removeNumberWithWrongIndex() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> numbers.removeAt(-1));
+        }
     }
 
-    @Test
-    void removeNumber() {
-        numbers.removeAt(0);
+    @Nested
+    class GetTest {
 
-        assertEquals(2, numbers.length());
-        assertEquals(2, numbers.get(0));
     }
 
-    @Test
-    void removeNumberOutOfArrayLength() {
-        assertAll(
-                () -> assertThrows(IllegalArgumentException.class, () -> numbers.removeAt(10)),
-                () -> assertEquals(3, numbers.length())
-        );
-    }
+    @Nested
+    class GetIndexTest {
+        @Test @Tag("Smoke") @Tag("Regression")
+        void getIndexForExistNumber() {
+            assertEquals(0, numbers.indexOf(1));
+        }
 
-    @Test
-    void removeNumberWithWrongIndex() {
-        assertThrows(IllegalArgumentException.class,
-                () -> numbers.removeAt(-1));
-    }
-
-    @Test
-    void insertNumberAfterRemoveOne() {
-        numbers.removeAt(0);
-        numbers.insert(4);
-
-        assertAll(
-                () -> assertEquals(3, numbers.length()),
-                () -> assertEquals(4, numbers.get(2))
-        );
-    }
-
-    @Test
-    void getIndexForExistNumber() {
-        assertEquals(0, numbers.indexOf(1));
-    }
-
-    @Test
-    void getIndexForNotExistedNumber() {
-        assertEquals(-1, numbers.indexOf(10));
+        @Test @Tag("Regression")
+        void getIndexForNotExistedNumber() {
+            assertEquals(-1, numbers.indexOf(10));
+        }
     }
 }
